4c996cb80af325db347fad8a4e320842a152487b
Henrique Generoso <henrique.generoso@gmail.com>
2025-04-02 20:38:39 -0300

    erro na Matrix

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -74,3 +74,23 @@ describe("cypress simulator - consentimento de cookies", () => {
       .should("eq", "declined")
   })
 })
+
+describe("Erro na Matrix", () => {
+  beforeEach(() => {
+    cy.login()
+
+    cy.visit("./src/index.html?skipCaptcha=true&chancesOfError=1", {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("cookieConsent", "accepted")
+      }
+    })
+  })
+
+  it("deve retornar um erro na Matrix", () => {
+    cy.run("cy.visit")
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "There's a glitch in the Matrix.")
+      .and("be.visible")
+  })
+})
